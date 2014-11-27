4ec326d9a40c31afce9b22c01d328f1ba24248d5
mounthief <mounthief>
2014-11-27 10:01:16 +0000

    Initial commit

---
+++ wakProj1/WebFolder/affect_dossier_agence.waPage/scripts/index.js
@@ -0,0 +1,19 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var affecter = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	affecter.click = function affecter_click (event)// @startlock
+	{// @endlock
+		sources.dossier.pour_agence.set( sources.agence ); 
+   		sources.dossier.save();
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("affecter", "click", affecter.click, "WAF");
+// @endregion
+};// @endlock
